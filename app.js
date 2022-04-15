b5dc53b50c73bf8d8e58f5446e64ea8d73353e3b
accinn <aylinpt02@gmail.com>
2022-04-15 14:30:29 -0300

    icons and style

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 //const path = require('path')
-let part = 3000;
+let part = 8000;
 //app.use(express.static('public')); // para acceder a sus archivos sin especificar la carpeta, o sea que queda estatico (se debe agregar antes de todos los links)
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.resolve(__dirname,"./public")));
 
 // app.get('*',(req,res))
 app.get('/', (req, res) => {
